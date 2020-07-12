083b53e09ec6fe072cd77b081916834099698248
Anunayj <anunay.j1@gmail.com>
2020-07-13 02:11:59 +0530

    Demo Data

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import config
 import mysql.connector
 import sys
+from books import booklist
 
 
+def InsertBook(bookdata):
+    sql = f"insert into {config.mysql['table']} (Booktitle,Author,Price,Genre) values(%s,%s,%s,%s)"
+    dataCursor.execute(sql, bookdata)
+    con.commit()
+ 
+def PopulateDB():
+    for book in booklist:
+        bookdata = book[:2] + (int(book[2]),) + book[3:]
+        InsertBook(bookdata)
 
-
-def setupTable(cursor):
-    cursor.execute("SHOW TABLES LIKE %s",(config.mysql["table"],))
-    result = cursor.fetchone()
+def setupTable(populate = False):
+    dataCursor.execute("SHOW TABLES LIKE %s",(config.mysql["table"],))
+    result = dataCursor.fetchone()
     if result:
         print(f"Using table {config.mysql['table']}")
         pass
         #TODO: Check Correct Schema
     else:
         print("Table not Found, Setting up a new table")
-        cursor.execute(f"CREATE TABLE {config.mysql['table']} (Booknumber int UNSIGNED PRIMARY KEY AUTO_INCREMENT,Booktitle varchar(255),Author varchar(255),Price int , Genre varchar(50))")
-
+        dataCursor.execute(f"CREATE TABLE {config.mysql['table']} (Booknumber int UNSIGNED PRIMARY KEY AUTO_INCREMENT,Booktitle varchar(255),Author varchar(255),Price int , Genre varchar(50))")
+        if(populate):
+            PopulateDB()
 
 try:
     con = mysql.connector.connect(
@@ -31,8 +41,7 @@ except Exception as e:
 print("Connection Successful")
 dataCursor = con.cursor()
 
-setupTable(dataCursor)
-print("pls save me")
+setupTable(dataCursor, populate=True)
 
 
 
